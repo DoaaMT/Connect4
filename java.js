e5b1728268db6e58ef6a8fa1e362be737d23fde3
Doaa Turkistani <doaa.turkistani@hotmail.com>
2019-12-04 17:24:30 +0300

    fixed

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ window.onload = function () {
     Swal.fire({
         title: "Welcome to Connect 4 game.. ",
         text: "Let's see who wins!!!",
-        imageUrl: "./Pic/c.JPG" ,
+        imageUrl: "Pic/c.JPG" ,
         imageWidth: 300,
         imageHeight: 310,
         imageAlt: 'Welcome image',
